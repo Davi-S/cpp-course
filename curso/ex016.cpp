9c89a30e6124a493442bcdb71b3c78e2a1b2fb22
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-05-28 18:43:57 -0300

    Add files via upload

--- curso/ex016.cpp
+++ curso/ex016.cpp
@@ -1,1 +1,15 @@
 // Create a program that reads a decimal number and displays its entire portion on the screen
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float input, entire;
+    cout << "Type a decimal number:\n$ ";
+    cin >> input;
+    modf(input, &entire); // using cmath library
+    cout << "The entire part of this number is " << entire;
+    return 0;
+}
---
+++ curso/ex018.cpp
@@ -0,0 +1,2 @@
+// Write a program that reads any angle on the screen
+// and displays the value of the sine, cosine and tangent of that angle
