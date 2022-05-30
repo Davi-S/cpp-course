d174dc9ccf2c99742b283926a6b24f745b4d7258
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-05-30 12:33:05 -0300

    Delete curso directory

--- curso/ex001.cpp
+++
@@ -1,9 +0,0 @@
-// Make a program that prints "Hello Wolrd" on the screen
-#include <iostream>
-using namespace std;
-
-int main(int argc, char const *argv[])
-{
-    cout<<"Hello World";
-    return 0;
-} 
