7405c4bcaccc226546370a33a28b0b2c9a47c8ed
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-06-01 21:42:46 -0300

    completed exercises
    023
    024

--- aula009/ex023.cpp
+++ aula009/ex023.cpp
@@ -1,1 +1,20 @@
-// Write a program that reads a number from 0 to 9999 and displays each of the separate digits on the screen.
+// Write a program that reads a integer number from 0 to 9999
+// and displays each of the separate digits on the screen.
+ 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char str_number[4];
+    cout << "Type a integer number from 0 to 9999:\n$ ";
+    cin >> str_number;
+
+    cout << "Thousands: " << str_number[0] << "000\n";
+    cout << "Hundreds: " << str_number[1] << "00\n";
+    cout << "Dozens: " << str_number[2] << "0\n";
+    cout << "Units: " << str_number[3] << "\n";
+
+    return 0;
+}
--- aula009/ex024.cpp
+++ aula009/ex024.cpp
@@ -1,1 +1,25 @@
 // Create a program that reads the name of a city and tells whether or not it starts with "San"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string city;
+    cout <<  "Type the city name:\n$ ";
+    cin >> city; // only collect input before space
+    for (auto & c: city) c = tolower(c);
+
+    if (city == "san")
+    {
+        cout << "City does starts with \"San\"\n";
+    }
+    
+    else
+    {
+        cout << "City does not starts with \"San\"\n";
+    }
+
+    return 0;
+}
