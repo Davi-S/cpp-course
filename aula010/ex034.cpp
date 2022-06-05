a3dc6bf95fe413f83194531e40c95adec4152689
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-06-05 13:01:17 -0300

    exercises completed

--- aula010/ex034.cpp
+++ aula010/ex034.cpp
@@ -1,3 +1,26 @@
 // Create a program that reads an employee's salary and calculates the value of his raise.
 // For salaries greater than $1,250.00, calculate a 10% increase.
 // For those below or equal, the increase is 15%
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    float salary, modified_salary;
+    cout << "Type the employee actual salary:\nU$"; 
+    cin >> salary;
+
+    if (salary > 1250)
+    {
+        modified_salary = salary * 1.1; // 10% raise
+    }
+    else
+    {
+        modified_salary = salary * 1.5; // 15% raise
+    }
+
+    cout << "The salary after the raise is: " << modified_salary;
+
+    return 0;
+}
--- aula010/ex035.cpp
+++ aula010/ex035.cpp
@@ -1,1 +1,21 @@
 // Create a program that reads the length of three lines and tells whether or not they can form a triangle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int side_1, side_2, side_3;
+    cout << "Type the 3 sides to see if the triangle exists:\n$ ";
+    cin >> side_1 >> side_2 >> side_3;
+
+    if (side_1 + side_2 > side_3 && side_2 + side_3 > side_1 && side_3 + side_1 > side_2)
+    {
+        cout << "The triangle exists!";
+    }
+    else 
+    {
+        cout << "The triangle does not exists!";
+    }
+    return 0;
+}
