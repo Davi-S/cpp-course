33c0625a84a36c95f21fbe65f10ac2577d5ff130
Davi S <67160664+Davi-S@users.noreply.github.com>
2022-06-23 23:10:01 -0300

    completed exercises

--- aula012/ex044.cpp
+++ aula012/ex044.cpp
@@ -1,4 +1,5 @@
-// Create a program that calculates the amount to be paid for a product considering its normal price and payment terms
+// Create a program that calculates the amount to be paid for a product
+// considering its normal price and payment terms
 // - cash: 10% discount
 // - the view on the card: 5% discount
 // - up to 2x on the card: normal price
@@ -13,7 +14,34 @@ int main()
     cout << "Item normal price: ";
     cin >> normal_price;
 
-    // TODO finish program
+    int payment_method;
+    do
+    {
+        cout << "Payment methods:\n";
+        cout << "[1] -> cash: 10% discount\n[2] -> cash by card: 5% discount\n[3] -> 2x card: normal price\n[4] 3x or more on card: 30% interest\n";
+        cout << "Your choice: ";
+        cin >> payment_method;
+    }
+    while (payment_method < 0 || payment_method > 4);
+
+    if (payment_method == 1)
+    {
+        normal_price -= normal_price * 0.1;
+    }
+    else if (payment_method == 2)
+    {
+        normal_price -= normal_price * 0.05;
+    }
+    else if (payment_method == 3)
+    {
+        // Normal price
+    }
+    else if (payment_method == 4)
+    {
+        normal_price += normal_price * 0.3;
+    }
+    
+    cout << "The price now is: " << normal_price;
 
     return 0;   
 }
--- aula012/ex045.cpp
+++ aula012/ex045.cpp
@@ -1,1 +1,60 @@
 // Create a program that makes the computer play rock paper scissors
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{   
+    // 1 == ROCK
+    // 2 == SCISSORS
+    // 3 == PAPER   
+
+    // computer choice
+    srand(time(0));
+    int computer_choice = rand() % 3 + 1; // range 1 to 3
+
+    // get user choice
+    int user_choice;
+    do
+    {
+        cout << "Make your choice:\n";
+        cout << "[1] -> rock\n[2] -> scissors\n[3] -> paper\n";
+        cout << "Your choice: ";
+        cin >> user_choice;
+    }
+    while (user_choice < 0 || user_choice > 3);
+
+
+    string result;
+    if (user_choice == computer_choice)
+    {
+        result = "draw";
+    }
+    else if ((user_choice == 1 && computer_choice == 2) || (user_choice == 2 && computer_choice == 3) || (user_choice == 3 && computer_choice == 1))
+    {
+        result = "won";
+    }
+    else
+    {
+        result = "lost";
+    }
+
+    cout << "The computer has chosen ";
+    if (computer_choice == 1)
+    {
+        cout << "rock";
+    }
+    else if (computer_choice == 2)
+    {
+        cout << "scissors";
+    }
+    else 
+    {
+        cout << "paper";
+    }
+    cout << "!!!\n";
+    cout << "You " << result << " the game!!!";
+
+    return 0;
+}
